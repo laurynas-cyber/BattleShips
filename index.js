cc5cc4fbbe481b8d591dc66c1ce790f63cfb9c0f
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-08-13 15:56:54 +0300

    changed locations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = 2000;
 
 app.use(express.static("public"));
 // app.use(bodyParser.json());
